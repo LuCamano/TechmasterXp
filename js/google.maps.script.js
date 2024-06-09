ca3b39ca57778450b3226aac68fac21a4915ba12
LuCamano <lu.camano@duocuc.cl>
2024-06-09 16:39:54 -0400

    first commit

---
+++ js/google.maps.script.js
@@ -0,0 +1,73 @@
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        // Coordenadas de Monterrey, N.L., México
+        center: {lat: -36.820111650166766, lng: -73.04419473263347}, //{lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // determina la posicion
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        /*
+         * Para fines de minimizar las adecuaciones debido a que es este una demostración de adaptación mínima de código, se reemplaza forEach por some.
+         */
+        // places.forEach(function(place) {
+        places.some(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+            // some interrumpe su ejecución en cuanto devuelve un valor verdadero (true)
+            return true;
+        });
+        map.fitBounds(bounds);
+    });
+}
---
+++ js/script.js
@@ -0,0 +1,203 @@
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+    })
+    })()
+
+$('#password1').after("<div class='invalid-feedback' id='clave1inv'>Debe ingresar una contraseña</div>");
+$('#password2').after("<div class='invalid-feedback' id='clave2inv'>Las contraseñas no coinciden</div>");
+$('#rut').after("<div class='invalid-feedback' id='rutinv'>RUT inválido</div>");
+$('#email').after("<div class='invalid-feedback' id='emailinv'>Debe ingresar un correo electrónico</div>");
+$('#nombre').after("<div class='invalid-feedback' id='nombreinv'>Debe ingresar un nombre</div>");
+$('#apellido').after("<div class='invalid-feedback' id='apellidoinv'>Debe ingresar un apellido</div>");
+$('#telefono').after("<div class='invalid-feedback' id='telefonoinv'>Debe ingresar un teléfono</div>");
+$('#direccion').after("<div class='invalid-feedback' id='direccioninv'>Debe ingresar una dirección</div>");
+
+function validar_clave(){
+    const clave1 = $("#password1")[0];
+    const mayus = /[A-Z]/.test(clave1.value);
+    const minus = /[a-z]/.test(clave1.value);
+    const num = /[0-9]/.test(clave1.value);
+    const special = /[^A-Za-z0-9]/.test(clave1.value);
+    const length = clave1.value.length >= 8;
+    if (!mayus) {
+        clave1.setCustomValidity('Debe ingresar al menos una mayúscula');
+        $("#clave1inv").text("Debe ingresar al menos una mayúscula");
+    } else if (!minus) {
+        clave1.setCustomValidity('Debe ingresar al menos una minúscula');
+        $("#clave1inv").text("Debe ingresar al menos una minúscula");
+    } else if (!num) {
+        clave1.setCustomValidity('Debe ingresar al menos un número');
+        $("#clave1inv").text("Debe ingresar al menos un número");
+    } else if (!special) {
+        clave1.setCustomValidity('Debe ingresar al menos un caracter especial');
+        $("#clave1inv").text("Debe ingresar al menos un caracter especial");
+    } else if (!length) {
+        clave1.setCustomValidity('La contraseña debe tener al menos 8 caracteres');
+        $("#clave1inv").text("La contraseña debe tener al menos 8 caracteres");
+    }
+
+    if (mayus && minus && num && special && length) {
+        clave1.setCustomValidity('');
+    }
+};
+
+function validar_passwords(){
+    const clave1 = $("#password1")[0];
+    const clave2 = $("#password2")[0];
+
+    if(clave1.value != clave2.value){
+        clave2.setCustomValidity('Las contraseñas no coinciden');
+        console.log("Las contraseñas no coinciden");
+    } else {
+        clave2.setCustomValidity('');
+        console.log("Las contraseñas coinciden");
+    }
+};
+
+$("#password1").on( "input", function () {
+    validar_clave();
+    validar_passwords();
+});
+
+$("#password2").on( "input", validar_passwords);
+
+$('#rut').on( "input", function () {
+    let rut = $("#rut")[0];
+    checkRut(rut);
+});
+
+
+function checkRut(rut) {
+    // Despejar Puntos
+    let valor = rut.value.replace(".", "");
+    // Despejar Guión
+    valor = valor.replace("-", "");
+    
+    valor = valor.replace(/[^0-9kK]/, '');
+
+    // Aislar Cuerpo y Dígito Verificador
+    cuerpo = valor.slice(0, -1);
+    dv = valor.slice(-1).toUpperCase();
+  
+    // Formatear RUN
+
+    rut.value = cuerpo + dv;
+  
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if (cuerpo.length < 7) {
+      rut.setCustomValidity("RUT Incompleto");
+      return false;
+    } else if (cuerpo.length > 8) {
+      rut.setCustomValidity("RUT Inválido");
+      return false;
+    }
+  
+    // Calcular Dígito Verificador
+    suma = 0;
+    multiplo = 2;
+  
+    // Para cada dígito del Cuerpo
+    for (i = 1; i <= cuerpo.length; i++) {
+      // Obtener su Producto con el Múltiplo Correspondiente
+      index = multiplo * valor.charAt(cuerpo.length - i);
+  
+      // Sumar al Contador General
+      suma = suma + index;
+  
+      // Consolidar Múltiplo dentro del rango [2,7]
+      if (multiplo < 7) {
+        multiplo = multiplo + 1;
+      } else {
+        multiplo = 2;
+      }
+    }
+  
+    // Calcular Dígito Verificador en base al Módulo 11
+    dvEsperado = 11 - (suma % 11);
+  
+    // Casos Especiales (0 y K)
+    dv = dv == "K" ? 10 : dv;
+    dv = dv == 0 ? 11 : dv;
+  
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if (dvEsperado != dv) {
+      rut.setCustomValidity("RUT Inválido");
+      return false;
+    }
+  
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    rut.setCustomValidity("");
+};
+
+function validar_email(){
+    const email = $("#email")[0];
+    const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!email_regex.test(email.value)) {
+        email.setCustomValidity('Email inválido');
+        $("#emailinv").text("Email inválido");
+    } else {
+        email.setCustomValidity('');
+        $("#emailinv").text("Debe ingresar un correo electrónico");
+    }
+    if (email.value == "") {
+        $("#emailinv").text("Debe ingresar un correo electrónico");
+    }
+};
+
+function validarTexto(texto, msgerror, textoError){
+    txt = texto.value;
+    if (txt.length < 3) {
+        texto.setCustomValidity('Debe ingresar al menos 3 caracteres');
+        msgerror.text("Debe ingresar al menos 3 caracteres");
+    } else {
+        texto.setCustomValidity('');
+    }
+    if (txt == "") {
+        msgerror.text(textoError);
+    };
+};
+
+$('#nombre').on('input', function () {
+    const nombre = $("#nombre")[0];
+    const nombreinv = $("#nombreinv");
+    const txtError = "Debe ingresar un nombre";
+    validarTexto(nombre, nombreinv, txtError);
+});
+
+$('#apellido').on('input', function () {
+    const apellido = $("#apellido")[0];
+    const apellidoinv = $("#apellidoinv");
+    const txtError = "Debe ingresar un apellido";
+    validarTexto(apellido, apellidoinv, txtError);
+});
+
+function validar_direccion(){
+    const direccion = $("#direccion")[0];
+    const direccioninv = $("#direccioninv");
+    let txt = direccion.value;
+    if (txt.length < 10) {
+        direccion.setCustomValidity('Debe ingresar al menos 10 caracteres');
+        direccioninv.text("Debe ingresar al menos 10 caracteres");
+        return false;
+    } else if (txt.split(" ").length < 2 && /\d/.test(txt)) {
+        direccion.setCustomValidity('Debe ingresar una dirección válida');
+        direccioninv.text("Debe ingresar una dirección válida");
+        return false;
+    }
+}
+
+$('#direccion').on('input', validar_direccion);
